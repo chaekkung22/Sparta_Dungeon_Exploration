3fc3aedbc47512f35107c3c084bd8a924b883465
chae_kkung <ycy0109@naver.com>
2025-05-23 01:45:05 +0900

    fix : 레이 발사 위치 변경

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement")]
-    [SerializeField] private float speed;
+    [Header("Movement")] [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
     [SerializeField] private LayerMask groundLayer;
     private Vector2 currentMovementInput;
 
-    [Header("Look")] 
-    [SerializeField] private Transform cameraContainer;
+    [Header("Look")] [SerializeField] private Transform cameraContainer;
     [SerializeField] private float minXLook;
     [SerializeField] private float maxXLook;
     [SerializeField] private float lookSensitivity;
@@ -26,16 +24,17 @@ public class PlayerController : MonoBehaviour
 
     public Action inventory;
     private Rigidbody _rigidbody;
+    private CapsuleCollider capsuleCollider;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
         animator = GetComponentInChildren<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
     }
 
     void Start()
     {
-        
     }
 
     void Update()
@@ -74,7 +73,7 @@ public class PlayerController : MonoBehaviour
         Vector3 dir = transform.forward * currentMovementInput.y + transform.right * currentMovementInput.x;
         dir *= speed;
         dir.y = _rigidbody.velocity.y;
-        
+
         _rigidbody.velocity = dir;
     }
 
@@ -83,10 +82,10 @@ public class PlayerController : MonoBehaviour
         camCurXRotation += mouseDelta.y * lookSensitivity;
         camCurXRotation = Mathf.Clamp(camCurXRotation, minXLook, maxXLook);
         cameraContainer.localEulerAngles = new Vector3(-camCurXRotation, 0, 0);
-        
+
         transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
     }
-    
+
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -122,19 +121,28 @@ public class PlayerController : MonoBehaviour
     {
         Ray[] rays = new Ray[4]
         {
-            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
-            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
-            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
-            new Ray(transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down)
+            new Ray(
+                transform.TransformPoint(capsuleCollider.center) - new Vector3(0, capsuleCollider.center.y, 0) + (transform.forward * 0.2f) +
+                (transform.up * 0.01f), Vector3.down),
+            new Ray(
+                transform.TransformPoint(capsuleCollider.center) - new Vector3(0, capsuleCollider.center.y, 0) + (-transform.forward * 0.2f) +
+                (transform.up * 0.01f), Vector3.down),
+            new Ray(
+                transform.TransformPoint(capsuleCollider.center) - new Vector3(0, capsuleCollider.center.y, 0) + (transform.right * 0.2f) +
+                (transform.up * 0.01f), Vector3.down),
+            new Ray(
+                transform.TransformPoint(capsuleCollider.center) - new Vector3(0, capsuleCollider.center.y, 0) + (-transform.right * 0.2f) +
+                (transform.up * 0.01f), Vector3.down)
         };
 
         for (int i = 0; i < rays.Length; i++)
         {
-            if (Physics.Raycast(rays[i], 0.15f, groundLayer))
+            if (Physics.Raycast(rays[i], 0.1f, groundLayer))
             {
                 return true;
             }
         }
+
         return false;
     }
 }
